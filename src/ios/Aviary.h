4b492e156584a2968f8cd49958b2b4f5ee3abfe9
m1is <git@m1is.com>
2014-01-01 22:42:12 -0800

    Added iOS

---
+++ src/ios/Aviary.h
@@ -0,0 +1,21 @@
+// AviaryGap - v.1.0.0
+// (c) 2013 Ryan Vanderpol, me@ryanvanderpol.com, MIT Licensed.
+// AviaryPlugin.h may be freely distributed under the MIT license.
+//
+//  AviaryPlugin.h
+//
+
+#import <UIKit/UIKit.h>
+#import <Cordova/CDVPlugin.h>
+#import "AFPhotoEditorController.h"
+
+@interface Aviary : CDVPlugin <AFPhotoEditorControllerDelegate> {
+    AFPhotoEditorController* aviary;
+}
+
+@property (nonatomic, retain) AFPhotoEditorController *aviary;
+@property (nonatomic, retain) NSString* pluginCallbackId;
+
+- (void) show:(CDVInvokedUrlCommand*)command;
+
+@end
---
+++ src/ios/Aviary.m
@@ -0,0 +1,191 @@
+
+#import "Aviary.h"
+#import <Cordova/CDVPlugin.h>
+#import "AFPhotoEditorController.h"
+
+@implementation Aviary
+
+@synthesize aviary;
+@synthesize pluginCallbackId;
+
+- (void) show:(CDVInvokedUrlCommand*) command
+{
+    pluginCallbackId = command.callbackId;
+    
+    NSURL* url = [NSURL URLWithString:arg0];
+    NSString* imagePath = [url path];
+    UIImage* image = [UIImage imageWithContentsOfFile:imagePath];
+    
+    // tool list
+    if (![[command.arguments objectAtIndex:3] isEqual:[NSNull null]])
+    {
+        NSArray* toolList = [command.arguments objectAtIndex:3];
+        NSMutableArray* tools = [[NSMutableArray alloc] init];
+        
+        for (id object in toolList)
+        {
+            if ([object isEqualToString:@"SHARPNESS"])
+            {
+                [tools addObject:kAFSharpness];
+            }
+            else if ([object isEqualToString:@"EFFECTS"])
+            {
+                [tools addObject:kAFEffects];
+            }
+            else if ([object isEqualToString:@"RED_EYE"])
+            {
+                [tools addObject:kAFRedeye];
+            }
+            else if ([object isEqualToString:@"CROP"])
+            {
+                [tools addObject:kAFCrop];
+            }
+            else if ([object isEqualToString:@"WHITEN"])
+            {
+                [tools addObject:kAFWhiten];
+            }
+            else if ([object isEqualToString:@"DRAWING"])
+            {
+                [tools addObject:kAFDraw];
+            }
+            else if ([object isEqualToString:@"STICKERS"])
+            {
+                [tools addObject:kAFStickers];
+            }
+            else if ([object isEqualToString:@"TEXT"])
+            {
+                [tools addObject:kAFText];
+            }
+            else if ([object isEqualToString:@"BLEMISH"])
+            {
+                [tools addObject:kAFBlemish];
+            }
+            else if ([object isEqualToString:@"MEME"])
+            {
+                [tools addObject:kAFMeme];
+            }
+            else if ([object isEqualToString:@"ADJUST"])
+            {
+                [tools addObject:kAFAdjustments];
+            }
+            else if ([object isEqualToString:@"ENHANCE"])
+            {
+                [tools addObject:kAFEnhance];
+            }
+            else if ([object isEqualToString:@"COLOR_SPLASH"])
+            {
+                [tools addObject:kAFSplash];
+            }
+            else if ([object isEqualToString:@"TILT_SHIFT"])
+            {
+                [tools addObject:kAFFocus];
+            }
+            else if ([object isEqualToString:@"ORIENTATION"])
+            {
+                [tools addObject:kAFOrientation];
+            }
+            else if ([object isEqualToString:@"FRAMES"])
+            {
+                [tools addObject:kAFFrames];
+            }
+        }
+        
+        [AFPhotoEditorCustomization setToolOrder:tools];
+    }
+    
+    self.aviary = [[AFPhotoEditorController alloc] initWithImage:image];
+    [self.aviary setDelegate:self];
+    [self.viewController presentModalViewController:self.aviary animated:YES];
+}
+
+-(void) photoEditor:(AFPhotoEditorController *)editor finishedWithImage:(UIImage *)image
+{
+    BOOL ok = false;
+    NSString * res = @"error";
+    float quality = 80.0f;
+    BOOL saveToPhotoAlbum = YES;
+    
+    NSData* data = nil;
+    data = UIImageJPEGRepresentation(image , quality);
+    
+    // write to temp directory and reutrn URI
+    // get the temp directory path
+    NSString* docsPath = [NSTemporaryDirectory() stringByStandardizingPath];
+    NSError* err = nil;
+    NSFileManager* fileMgr = [[NSFileManager alloc] init];
+    
+    // generate unique file name
+    NSString* filePath;
+    int i=1;
+    do
+    {
+        filePath = [NSString stringWithFormat:@"%@/photo_%03d.%@", docsPath, i++, @"jpg"];
+    }
+    while([fileMgr fileExistsAtPath: filePath]);
+    
+    // save file
+    if (![data writeToFile: filePath options: NSAtomicWrite error: &err])
+    {
+        res = @"error saving file";
+    }
+    else
+    {
+        res = [[NSURL fileURLWithPath: filePath] absoluteString];
+        ok = true;
+    }
+
+    if(ok)
+    {
+        if (saveToPhotoAlbum )
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+        
+        [self returnSuccess:res];
+    }
+    else
+    {
+        [self returnError:res];
+    }
+    
+    [self closeAviary];
+}
+
+-(void) photoEditorCanceled:(AFPhotoEditorController *)editor
+{
+    [self closeAviary];
+}
+
+
+-(void) returnSuccess:(NSString*)imagePath
+{
+    NSString* filename = [imagePath lastPathComponent];
+    
+    NSMutableDictionary* dict = [[NSMutableDictionary alloc]init];
+    [dict setObject:imagePath forKey:@"src"];
+    [dict setObject:filename forKey:@"name"];
+    
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsDictionary:dict];
+    
+    NSString* javaScript = [result toSuccessCallbackString:self.pluginCallbackId];
+    [self writeJavascript:javaScript];
+}
+
+-(void) returnError:(NSString*)message
+{
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:message];
+    
+    NSString* javaScript = [result toSuccessCallbackString:self.pluginCallbackId];
+    [self writeJavascript:javaScript];
+}
+
+
+-(void) closeAviary
+{
+    
+    [self.aviary dismissModalViewControllerAnimated:YES];
+    self.aviary.delegate = nil;
+    self.aviary = nil;
+}
+
+@end
